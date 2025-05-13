21b6d49ae441ab64321e3da278e10f0fddf67235
COQUTE <kimjiho246@naver.com>
2025-05-14 01:55:20 +0900

    DB, favicon, 기본 설정

---
+++ FeatherWorld/src/main/java/com/featherworld/project/main/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.featherworld.project.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/** 메인 페이지 컨트롤러
+ * @author Jiho
+ */
+@Controller
+public class MainController {
+	
+	/** 메인 페이지 forward 메서드
+	 * @author Jiho
+	 * @return src/main/resources/templates/common/main.html
+	 */
+	@RequestMapping("/")
+	public String mainPage() {
+		return "common/main";
+	}
+}
---
+++ FeatherWorld/src/main/java/com/featherworld/project/main/mapper/MainMapper.java
@@ -0,0 +1,8 @@
+package com.featherworld.project.main.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MainMapper {
+
+}
