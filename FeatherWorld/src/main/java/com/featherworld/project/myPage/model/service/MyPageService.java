f8df112c9dee833e4d73485b58b84ac91872c5d4
Logan-01-for <sjson0105@gmail.com>
2025-05-19 17:51:41 +0900

    0 5 1 9

--- FeatherWorld/src/main/java/com/featherworld/project/myPage/model/service/MyPageService.java
+++ FeatherWorld/src/main/java/com/featherworld/project/myPage/model/service/MyPageService.java
@@ -1,5 +1,29 @@
 package com.featherworld.project.myPage.model.service;
 
-public interface MyPageService {
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.featherworld.project.friend.model.service.IlchonService;
+
+
+@Service
+public class MyPageService {
+
+    @Autowired
+    private IlchonService ilchonService;
+
+    public Map<String, Object> getIlchonMemberList(int memberNo, int cp) {
+        return ilchonService.selectIlchonMemberList(memberNo, cp);
+    }
+
+    public int getIlchonCount(int memberNo) {
+        Map<String, Object> ilchonData = ilchonService.selectIlchonMemberList(memberNo, 1); // 1 = 첫 페이지
+        Integer count = (Integer) ilchonData.get("totalCount");
+        return count != null ? count : 0;
+    }
+
 
 }
+
