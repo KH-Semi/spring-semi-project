d011629636476b075e0854af6d084885501cc161
anjs1998 <anjs1998@gmail.com>
2025-06-04 12:52:37 +0900

    pagination 실시간 업데이트 안되는 오류 해결 + 내일촌 unfollow할떄 확인창 뜨게함

--- FeatherWorld/src/main/resources/static/js/friendList/incomingFriendListPart.js
+++ FeatherWorld/src/main/resources/static/js/friendList/incomingFriendListPart.js
@@ -23,18 +23,18 @@ const xIcon = (friend) => {
 };
 /*friend를 왼쪽으로 밀어버리는 애니메이션 동작함수 + 해당 요소를 DOM에서 제거*/
 const fadeOut = (friend) => {
-    return new Promise((resolve) => {
-      friend.classList.add("removing");
+  return new Promise((resolve) => {
+    friend.classList.add("removing");
 
-      // transition 완료 후 remove()
-      friend.addEventListener(
-        "transitionend",
-        () => {
-          friend.remove();
-          resolve();
-        },
-        { once: true }
-      );
+    // transition 완료 후 remove()
+    friend.addEventListener(
+      "transitionend",
+      () => {
+        friend.remove();
+        resolve();
+      },
+      { once: true }
+    );
   });
 };
 // DB 내부 일촌테이블 DELETE 전용 애니메이션 함수(fadeOut과는 반대방향으로 밀어버림) +  안에서 해당 요소를 DOM에서 제거.
@@ -42,19 +42,16 @@ const deleteFade = (friend) => {
   return new Promise((resolve) => {
     friend.classList.add("deleting");
 
-      // transition 완료 후 remove()
-      friend.addEventListener(
-        "transitionend",
-        () => {
-          friend.remove();
-          resolve();
-        },
-        { once: true }
-      );
-
-
+    // transition 완료 후 remove()
+    friend.addEventListener(
+      "transitionend",
+      () => {
+        friend.remove();
+        resolve();
+      },
+      { once: true }
+    );
   });
-  
 };
 /* changeTitleForSec()
   title : 동작을 수행할 웹페이지상 text 요소
@@ -69,8 +66,6 @@ const changeTitleForSec = (title, str) => {
         title.innerText = savedText;
         resolve();
       }, 1000);
-
-      
     } else {
       console.log("title이 null 입니다!");
       resolve();
@@ -91,6 +86,12 @@ let cancelBtn = document.getElementById("cancel-button");
 let sendFriendReqBtn = document.getElementById("send-friend-request-button");
 let friendSpans = document.querySelectorAll(".friend-item");
 let friendSendedSpans = document.querySelectorAll(".friend-item-sended");
+
+let currPagination = parseInt(document.getElementById("curr-pagination").value);
+let currPaginationFrom = parseInt(
+  document.getElementById("curr-pagination-from").value
+);
+
 document.getElementById("to-friendList-btn").addEventListener("click", () => {
   window.location.href = `/${memberNo}/friendList`;
 });
@@ -99,8 +100,11 @@ friendSpans.forEach(function (friend) {
   console.log(friend);
 });
 
+function isItemExists(item) {
+  // item : html 내부의 요소 List(querySelectorAll로 얻어온 요소 List)
 
-
+  return item.length != 0 ? true : false;
+}
 function updateModel(cp, cpFrom) {
   // "{memberNo:[0-9]+}/friendList/incoming" 에서 model만 업데이트
   //for pagination
@@ -117,11 +121,12 @@ function updateModel(cp, cpFrom) {
       return res.text();
     })
     .then((html) => {
-      console.log("html response:", html);
+      //console.log("html response:", html);
       // 응답받은 HTML을 파싱해서 DOM으로 변환
       const parser = new DOMParser();
       const doc = parser.parseFromString(html, "text/html");
-      console.log("doc: ", doc.body.innerText);
+
+      //console.log("doc: ", doc.body.innerText);
       return doc;
     });
 }
@@ -136,10 +141,14 @@ function resetEditCancelBtn() {
   friendSpans = document.querySelectorAll(".friend-item");
   friendSendedSpans = document.querySelectorAll(".friend-item-sended");
 
+  currPagination = parseInt(document.getElementById("curr-pagination").value);
+  currPaginationFrom = parseInt(
+    document.getElementById("curr-pagination-from").value
+  );
   //1. 현재 일촌 리스트로 돌아가기 버튼 click 이벤트 다시 추가
- 
+
   document.getElementById("to-friendList-btn").addEventListener("click", () => {
-  window.location.href = `/${memberNo}/friendList`;
+    window.location.href = `/${memberNo}/friendList`;
   });
   //3 // (내가 일촌신청을 받은사람 한정)프로필img / 이름 클릭시 헤당 user의 minihome으로
   friendSpans.forEach(function (friend) {
@@ -203,26 +212,52 @@ function resetEditCancelBtn() {
           }), // TO_NICKNAME/FROM_NICKNAME 판별 여부는 서버측에서 판단
         })
           .then((response) => response.json())
-          .then(async(data) => {
+          .then(async (data) => {
             console.log(data); // DEBUG용이므로 지우셔도 됩니다
-            if (data.status == 2 || data.status == 1) { // 2 : toNickname  수정성공, 1 : fromNickname 수정성공
-              console.log(data.status==2?"toNickname 수정 성공!":"fromNickname 수정 성공!");
+            if (data.status == 2 || data.status == 1) {
+              // 2 : toNickname  수정성공, 1 : fromNickname 수정성공
+              console.log(
+                data.status == 2
+                  ? "toNickname 수정 성공!"
+                  : "fromNickname 수정 성공!"
+              );
 
               await fadeOut(friend);
+
+              //model 업데이트 후 다시 items 갱신
+              console.log(currPagination, "+", currPaginationFrom);
+              // 현재 페이지네이션에 유저가 존재하거나, 현재 page가 1일떄.
+              let newItems = null;
+              if (
+                isItemExists(document.querySelectorAll(".friend-item")) ||
+                currPagination == 1
+              ) {
+                console.log("if 분기 작동");
+                newItems = await updateModel(
+                  currPagination,
+                  currPaginationFrom
+                );
+              } else {
+                // 현재 페이지네이션에 더이상 아무런 유저가 존재하지 않을때(현재 page가 1일떄는 제외).
+                // -> 대신 이전 Pagination의 정보를 가져옴
+                console.log("else 분기 작동");
+                newItems = await updateModel(
+                  currPagination - 1,
+                  currPaginationFrom
+                );
+              }
+              const currentBlock = document.querySelector(".main-content");
+              console.log(newItems);
+              if (currentBlock) {
+                currentBlock.innerHTML =
+                  newItems.body.firstElementChild.outerHTML;
+                resetEditCancelBtn();
+              }
               await changeTitleForSec(
                 document.getElementById("incoming-title"),
                 "새 일촌을 수락했습니다!! 잘부탁드려요!"
               );
-              //model 업데이트 후 다시 items 갱신
-              console.log(currPagination,"+", currPaginationFrom)
-              const newItems = await updateModel(currPagination, currPaginationFrom);
-              const currentBlock = document.querySelector(".main-content");
-              console.log(newItems);
-              if (currentBlock) {
-                currentBlock.innerHTML = newItems.body.firstElementChild.outerHTML;
-                resetEditCancelBtn();
-              }
-            } else  {
+            } else {
               console.log("수정 실패!");
               xIcon(friend);
               return Promise.reject("닉네임 수정 실패");
@@ -231,8 +266,8 @@ function resetEditCancelBtn() {
           .catch((err) => {
             console.error(err);
           });
-          
-              //
+
+        //
       });
       //accept 버튼 누른후 nickname 수정요청 + is_ilchon = 'Y'로 변경
       fromNicknameInput?.addEventListener("input", (e) => {
@@ -272,15 +307,44 @@ function resetEditCancelBtn() {
             }),
           })
             .then((response) => response.json())
-            .then((data) => {
+            .then(async (data) => {
               console.log(data); // DEBUG용이므로 지우셔도 됩니다
               if (data.status == 1) {
                 console.log("삭제 성공!");
+
+                await deleteFade(friend); // 일촌테이블 DELETE 전용 동작함수. 안에서 friend.remove()를 자체적으로 수행함.
+
+                let newItems = null;
+                if (
+                  isItemExists(document.querySelectorAll(".friend-item")) ||
+                  currPagination == 1
+                ) {
+                  console.log("if 분기 작동");
+                  newItems = await updateModel(
+                    currPagination,
+                    currPaginationFrom
+                  );
+                } else {
+                  // 현재 페이지네이션에 더이상 아무런 유저가 존재하지 않을때(현재 page가 1일떄는 제외).
+                  // -> 대신 이전 Pagination의 정보를 가져옴
+                  console.log("else 분기 작동");
+                  newItems = await updateModel(
+                    currPagination - 1,
+                    currPaginationFrom
+                  );
+                }
+                const currentBlock = document.querySelector(".main-content");
+                console.log(newItems);
+                // 현재 내용을 실시간 업데이트된 model안의 정보로 페이지에 show.
+                if (currentBlock) {
+                  currentBlock.innerHTML =
+                    newItems.body.firstElementChild.outerHTML;
+                  resetEditCancelBtn();
+                }
                 changeTitleForSec(
                   document.getElementById("incoming-title"),
                   "일촌신청을 거절했습니다."
                 );
-                deleteFade(friend); // 일촌테이블 DELETE 전용 동작함수. 안에서 friend.remove()를 자체적으로 수행함.
               } else if (data.status == 0) {
                 console.log("삭제 실패! 0");
 
@@ -335,7 +399,7 @@ function resetEditCancelBtn() {
             }),
           })
             .then((response) => response.json())
-            .then((data) => {
+            .then(async (data) => {
               console.log(data); // DEBUG용이므로 지우셔도 됩니다
               if (data.status == 1) {
                 console.log("삭제 성공!");
@@ -345,8 +409,47 @@ function resetEditCancelBtn() {
                 data.Ilchon.toNickname;
             } //refresh*/
 
-                checkIcon(friend);
-                deleteFade(friend); // 일촌테이블 DELETE 전용 동작함수. 안에서 friend.remove()를 자체적으로 수행함.
+                await checkIcon(friend);
+                await deleteFade(friend); // 일촌테이블 DELETE 전용 동작함수. 안에서 friend.remove()를 자체적으로 수행함.
+                let newItems = null;
+                console.log(
+                  isItemExists(document.querySelectorAll(".friend-item-sended"))
+                );
+                if (
+                  isItemExists(
+                    document.querySelectorAll(".friend-item-sended")
+                  ) ||
+                  currPaginationFrom === 1
+                ) {
+                  console.log(
+                    ".friend-item-sended:",
+                    isItemExists(
+                      document.querySelectorAll(".friend-item-sended")
+                    )
+                  );
+                  console.log(currPaginationFrom);
+                  console.log("if 분기 작동");
+                  newItems = await updateModel(
+                    currPagination,
+                    currPaginationFrom
+                  );
+                } else {
+                  // 현재 페이지네이션에 더이상 아무런 유저가 존재하지 않을때(현재 page가 1일떄는 제외).
+                  // -> 대신 이전 Pagination의 정보를 가져옴
+                  console.log("else 분기 작동");
+                  newItems = await updateModel(
+                    currPagination,
+                    currPaginationFrom - 1
+                  );
+                }
+                const currentBlock = document.querySelector(".main-content");
+                //console.log(newItems);
+                // 현재 내용을 실시간 업데이트된 model안의 정보로 페이지에 show.
+                if (currentBlock) {
+                  currentBlock.innerHTML =
+                    newItems.body.firstElementChild.outerHTML;
+                  resetEditCancelBtn();
+                }
               } else if (data.status == 0) {
                 console.log("삭제 실패! 0");
                 /* e.target.value = data.Ilchon.fromNickname;
@@ -385,11 +488,11 @@ function updateFriendListIncoming(cp, cpFrom) {
       return res.text();
     })
     .then((html) => {
-      console.log("html response:", html);
+      //console.log("html response:", html);
       // 응답받은 HTML을 파싱해서 DOM으로 변환
       const parser = new DOMParser();
       const doc = parser.parseFromString(html, "text/html");
-      console.log("doc: ", doc.body.innerText);
+      //console.log("doc: ", doc.body.innerText);
       // 기존 DOM의 요소 교체
       const currentBlock = document.querySelector(".main-content");
 
@@ -401,8 +504,6 @@ function updateFriendListIncoming(cp, cpFrom) {
     .catch((err) => console.error("fragment 갱신 실패:", err));
 }
 
-
-
 async function updateFriendListIncoming_refactored(cp, cpFrom) {
   //for pagination
   console.log("cp: ", cp, "cpFrom:", cpFrom);
@@ -410,7 +511,7 @@ async function updateFriendListIncoming_refactored(cp, cpFrom) {
   try {
     const doc = await updateModel(cp, cpFrom);
 
-    console.log("doc: ", doc.body.innerText);
+    //console.log("doc: ", doc.body.innerText);
     // 기존 DOM의 요소 교체
     const currentBlock = document.querySelector(".main-content");
 
